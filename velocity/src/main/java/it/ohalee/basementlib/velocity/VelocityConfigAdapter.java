57684b42b142f8215e970a8e331e8cb672670878
ohAlee <alebartoh@gmail.com>
2023-09-07 12:08:01 +0200

    Added config changes to velocity

--- velocity/src/main/java/it/ohalee/basementlib/velocity/VelocityConfigAdapter.java
+++ velocity/src/main/java/it/ohalee/basementlib/velocity/VelocityConfigAdapter.java
@@ -12,7 +12,6 @@ package it.ohalee.basementlib.velocity;
 
 import it.ohalee.basementlib.api.config.generic.adapter.ConfigurateConfigAdapter;
 import it.ohalee.basementlib.api.config.generic.adapter.ConfigurationAdapter;
-import it.ohalee.basementlib.api.plugin.BasementPlugin;
 import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
 import ninja.leaping.configurate.yaml.YAMLConfigurationLoader;
@@ -21,8 +20,13 @@ import java.nio.file.Path;
 
 public class VelocityConfigAdapter extends ConfigurateConfigAdapter implements ConfigurationAdapter {
 
-    public VelocityConfigAdapter(BasementPlugin plugin, Path path) {
+    private final BasementVelocity plugin;
+    private final Path path;
+
+    public VelocityConfigAdapter(BasementVelocity plugin, Path path) {
         super(plugin, path);
+        this.plugin = plugin;
+        this.path = path;
     }
 
     @Override
@@ -30,4 +34,19 @@ public class VelocityConfigAdapter extends ConfigurateConfigAdapter implements C
         return YAMLConfigurationLoader.builder().setPath(path).build();
     }
 
+    @Override
+    public Object set(String path, Object obj, boolean save) {
+        ConfigurationNode node = resolvePath(path);
+        node.setValue(obj);
+        if (save) {
+            save(node);
+            reload();
+        }
+        return obj;
+    }
+
+    @Override
+    public void update(Class<?> loader) {
+        this.plugin.update(loader, path.toFile().getName(), this);
+    }
 }
