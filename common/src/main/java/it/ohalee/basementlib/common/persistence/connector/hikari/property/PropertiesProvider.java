4edbe2aef145e45bbc6da134455c5530b9c84c53
ohAlee <alebartoh@gmail.com>
2024-05-06 19:09:13 +0200

    SSL false

--- common/src/main/java/it/ohalee/basementlib/common/persistence/connector/hikari/property/PropertiesProvider.java
+++ common/src/main/java/it/ohalee/basementlib/common/persistence/connector/hikari/property/PropertiesProvider.java
@@ -10,6 +10,10 @@ public class PropertiesProvider {
         for (PropertyPair propertyPair : pairs) {
             properties.setProperty(propertyPair.getProperty().toString(), propertyPair.getValue());
         }
+        properties.setProperty("useSSL", "false");
+        properties.setProperty("verifyServerCertificate", "false");
+        properties.setProperty("useUnicode", "true");
+        properties.setProperty("characterEncoding", "utf8");
     }
 
     public Properties getBuild() {
